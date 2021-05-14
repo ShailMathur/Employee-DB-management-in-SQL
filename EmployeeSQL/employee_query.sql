2c4cee73e1035bdb452cdb9c5d95b5045abd18a8
ShailMathur <79940751+ShailMathur@users.noreply.github.com>
2021-05-13 21:48:11 -0400

    Assignment

--- EmployeeSQL/employee_query.sql
+++ EmployeeSQL/employee_query.sql
@@ -46,6 +46,9 @@ CREATE TABLE dept_manager(
 
 --1. List the following details of each employee: employee number, last name, first name, sex, and salary.
 
+SELECT * FROM employees;
+
+
 SELECT e.emp_no, e.first_name, e.last_name, e.sex, s.salary 
 FROM employees AS e
 JOIN salaries AS s
